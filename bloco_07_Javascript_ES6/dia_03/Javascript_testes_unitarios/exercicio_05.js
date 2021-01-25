72755bc9b13d95f71ef3be3d12c984d4ec255736
Diego Vini <analistamm@gmail.com>
2021-01-25 08:00:36 -0300

    Testar se retorno de erro funciona

---
+++ bloco_07_Javascript_ES6/dia_03/Javascript_testes_unitarios/exercicio_05.js
@@ -0,0 +1,15 @@
+//Testar retorno de mensagem de erro
+
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+assert.throws(() => {
+    sum(4, '5');
+}, /^Error: parameters must be numbers$/)
