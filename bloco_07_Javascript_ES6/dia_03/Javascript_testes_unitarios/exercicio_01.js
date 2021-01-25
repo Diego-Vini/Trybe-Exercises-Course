be1c24f92eb6c194ed758c3c151c1658103a3c76
Diego Vini <analistamm@gmail.com>
2021-01-25 07:33:15 -0300

    Teste se é uma função

--- bloco_07_Javascript_ES6/dia_03/Javascript_testes_unitarios/exercicio_01.js
+++ bloco_07_Javascript_ES6/dia_03/Javascript_testes_unitarios/exercicio_01.js
@@ -1,1 +1,12 @@
-//Inicio
+//Retornar se é uma função
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+assert.strictEqual(typeof(sum), 'function');
