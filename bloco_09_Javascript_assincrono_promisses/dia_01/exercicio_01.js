6bff113120a781b78fa588ecebc3950da2123d19
Diego Vini <analistamm@gmail.com>
2021-01-27 19:20:05 -0300

    Comentando sequencia a ser impressa

--- bloco_09_Javascript_assincrono_promisses/dia_01/exercicio_01.js
+++ bloco_09_Javascript_assincrono_promisses/dia_01/exercicio_01.js
@@ -1,1 +1,34 @@
-//Inicio
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+console.log(planetDistanceFromSun(venus)); // B
+console.log(planetDistanceFromSun(jupiter)); // C
+
+//1º Objeto mars
+//2º Objeto venus
+//3º Objeto jupter
