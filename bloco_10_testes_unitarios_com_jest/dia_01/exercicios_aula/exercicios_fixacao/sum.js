2318b503a12874b08b44d4b71e60c5049ae89e83
Diego Vini <analistamm@gmail.com>
2021-02-03 19:08:21 -0300

    Primeiro teste soma dois numeros

--- bloco_10_testes_unitarios_com_jest/dia_01/exercicios_aula/exercicios_fixacao/sum.js
+++ bloco_10_testes_unitarios_com_jest/dia_01/exercicios_aula/exercicios_fixacao/sum.js
@@ -1,7 +1,10 @@
-function sum (a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error('parameters must be')
-    }
-    return a + b
-} ;
 
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+module.exports = sum;
